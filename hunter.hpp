c13345843b643e093cec7e4be89c631137af9303
alai031 <alai031@ucr.edu>
2020-11-24 15:31:34 -0800

    "Made changes to hunter.hpp and hunter.cpp"
    * Added attackMessage() for hunter

--- hunter.hpp
+++ hunter.hpp
@@ -1,21 +1,12 @@
 #ifndef HUNTER_HPP
-#define KNIGHT_HPP
+#define HUNTER_HPP
 
-//#include "Adventurer.hpp"
-#include "weapon.hpp"
+#include "Adventurer.hpp"
 
 class Hunter : public Adventurer(){
         public:
-                Hunter(int xp, int health, int dmg, int str) : xp(xp), health(health), damage(damage), strength(strength), weapon(nullptr){};
-                void set_weapon(Weapon* w);
-                void change_health(int change);
-                void change_damage(int change);
-                void change_strength(int change);
-		Weapon* get_weapon();
-                int get_health();
-
-        private:
-                Weapon* weapon;
+                Hunter() : health(200), damage(15){};
+                void attackMessage();
 };
 
 
