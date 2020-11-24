a2b05de81171ed6affc460af5636d741c09b9477
briortega <briortega14@gmail.com>
2020-11-24 15:14:52 -0800

    Made changes to the adventurer class

--- adventurer.cpp
+++ adventurer.cpp
@@ -1,35 +1,25 @@
 #include "adventurer.hpp"
 
-Adventurer::Adventurer(int xp, int health, int damage, int strength)
+Adventurer::Adventurer(int health, int damage)
 {
-	this->xp = xp;
 	this->health = health;
 	this->damage = damage;
-	this->strength = strength;
-}
-
-void Adventurer::set_weapon()
-{
-
 }
 
 void Adventurer::change_health(int healthChange)
 {
-	health += healthChange;
+	health -= healthChange;
 }
 
-void Adventurer::change_damage(int damageChange)
+void Adventurer::attackDamage()
 {
-	damage += damageChange;
-}
-
-void Adventurer::change_strength(int strengthChange)
-{
-	strength += strengthChange;
+	this->damage = damage;
 }
 
 int Adventurer::get_health()
 {
 	return health;
 }
+void Adventurer::attackMessage(){
 
+}
--- adventurer.hpp
+++ adventurer.hpp
@@ -3,16 +3,13 @@
 
 class Adventurer{
 protected:
-	int xp;
 	int health;
 	int damage;
-	int strength;
 public:
-	Adventurer(int xp, int health, int damage, int strength);
-	virtual void set_weapon();
-	virtual void change_health(int healthChange);
-	virtual void change_damage(int damageChange);
-	virtual void change_strength(int strengthChange);
+	Adventurer(int health, int damage);
+	virtual void attackMessage();
+	void change_health(int healthChange);
+	void attackDamage();
 	int get_health();
 
 };
