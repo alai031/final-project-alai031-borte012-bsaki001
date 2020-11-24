651efa7df8c4944ca3967b058986a740c3b02a64
alai031 <alai031@ucr.edu>
2020-11-24 15:25:08 -0800

    Revert "Made changes to hunter.hpp and hunter.cpp files"
    This reverts commit 88520587d4f7070dec5e63d7058275107344626e.

--- hunter.hpp
+++ hunter.hpp
@@ -2,12 +2,20 @@
 #define KNIGHT_HPP
 
 //#include "Adventurer.hpp"
+#include "weapon.hpp"
 
 class Hunter : public Adventurer(){
         public:
-                Hunter() : health(200), damage(15){};
-		void attackOutput(); //What the hunter says after he attacks
+                Hunter(int xp, int health, int dmg, int str) : xp(xp), health(health), damage(damage), strength(strength), weapon(nullptr){};
+                void set_weapon(Weapon* w);
+                void change_health(int change);
+                void change_damage(int change);
+                void change_strength(int change);
+		Weapon* get_weapon();
+                int get_health();
 
+        private:
+                Weapon* weapon;
 };
 
 
