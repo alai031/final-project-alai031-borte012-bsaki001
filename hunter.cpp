88520587d4f7070dec5e63d7058275107344626e
alai031 <alai031@ucr.edu>
2020-11-24 15:07:24 -0800

    Made changes to hunter.hpp and hunter.cpp files
    * Added attackOutput statement that hunter will say when attacking an enemy

--- hunter.cpp
+++ hunter.cpp
@@ -1,39 +1,9 @@
 #include "hunter.hpp"
 #include "Adventurer.hpp"
+#include <iostream>
+using namespace std;
 
-void Hunter::set_weapon(Weapon* w){
-	weapon = w;
-};
-
-void Hunter::change_health(int change){
-	health += change;
-	if (health < 0){
-		health = 0;
-	}
-};
-
-void Hunter::change_damage(int change){
-	damage += change;
-	if (damage < 0){
-		damage = 0;
-	}
-};
-
-void Hunter::change_strength(int change){
-	strength += change;
-	if (strenghth < 0){
-		strength = 0;
-	}
-};
-
-Weapon* Hunter::get_weapon(){
-	if (weapon == nullptr){
-		throw runtime_error("No equipped weapon");
-	}
-	return weapon;
-};
-
-int Hunter::get_health(){
-	return health;
+void hunter::attackOutput(){
+	cout << "Take this! The hunter stabs the enemy using his knife." << endl;
 };
 
--- hunter.hpp
+++ hunter.hpp
@@ -2,20 +2,12 @@
 #define KNIGHT_HPP
 
 //#include "Adventurer.hpp"
-#include "weapon.hpp"
 
 class Hunter : public Adventurer(){
         public:
-                Hunter(int xp, int health, int dmg, int str) : xp(xp), health(health), damage(damage), strength(strength), weapon(nullptr){};
-                void set_weapon(Weapon* w);
-                void change_health(int change);
-                void change_damage(int change);
-                void change_strength(int change);
-		Weapon* get_weapon();
-                int get_health();
+                Hunter() : health(200), damage(15){};
+		void attackOutput(); //What the hunter says after he attacks
 
-        private:
-                Weapon* weapon;
 };
 
 
