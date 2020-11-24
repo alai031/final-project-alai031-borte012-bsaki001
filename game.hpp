894468f4dc7555b940f7cfe6b880891c1e1cb029
alai031 <alai031@ucr.edu>
2020-11-24 15:58:52 -0800

    Added game.hpp and game.cpp files

---
+++ game.hpp
@@ -0,0 +1,19 @@
+#ifndef GAME_CLASS
+#define GAME_CLASS
+#include "adventurer.hpp"
+
+class Game() {
+	public:
+		static Game* Instance();
+
+	protected:
+		Game() : adventurer(nullptr); 
+
+	private:
+		static Game* _instance;
+		Adventurer* adventurer;
+
+};
+
+
+#endif
