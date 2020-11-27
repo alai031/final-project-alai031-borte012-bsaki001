4a1209e130b4eb4667ff05d0671fc7b428b91c9e
alai031 <alai031@ucr.edu>
2020-11-27 13:23:21 -0800

    "Made changes to unit tests for Game, Knight, and Hunter classes"
    * The unit tests now involve using actual classes instead of mock classes

--- hunter_test.hpp
+++ hunter_test.hpp
@@ -9,12 +9,13 @@
 
 TEST(HunterTest, Constructor) {
     Adventurer* adventurer = new Hunter();
-    EXPECT_EQ(adventurer->getHealth(), 200);
+    EXPECT_EQ(adventurer->get_health(), 200);
 }
 
 TEST(HunterTest, attackMessageOutputted) {
     Adventurer* adventurer = new Hunter();
-    monsterMock* monster = new monsterMock(20);
+    Monster* monster = new Monster(adventurer);
+    monster->setHealth(20);
     adventurer->attackDamage(monster);
     adventurer->attackMessage();
     EXPECT_EQ(monster->getHealth(), 5);   
@@ -22,14 +23,16 @@ TEST(HunterTest, attackMessageOutputted) {
 
 TEST(HunterTest, attackDamageMonsterLives) {
     Adventurer* adventurer = new Hunter();
-    monsterMock* monster = new monsterMock(40);
+    Monster* monster = new Monster(adventurer);
+    monster->setHealth(40);
     adventurer->attackDamage(monster);
     EXPECT_EQ(monster->getHealth(), 25);
 }
 
 TEST(HunterTest, attackDamageMonsterDies) {
     Adventurer* adventurer = new Hunter();
-    monsterMock* monster = new monsterMock(5);
+    Monster* monster = new Monster(adventurer);
+    monster->setHealth(5);
     adventurer->attackDamage(monster);
     EXPECT_EQ(monster->getHealth(), 0); 
 }
@@ -37,49 +40,49 @@ TEST(HunterTest, attackDamageMonsterDies) {
 TEST(HunterTest, changeHealthWithPositiveArg) {
     Adventurer* adventurer = new Hunter();
     adventurer->change_health(40);
-    EXPECT_EQ(adventurer->getHealth(), 240);
+    EXPECT_EQ(adventurer->get_health(), 240);
 }
 
 TEST(HunterTest, changeHealthWithNegativeArg) {
     Adventurer* adventurer = new Hunter();
     adventurer->change_health(-50);
-    EXPECT_EQ(adventurer->getHealth(), 150);
+    EXPECT_EQ(adventurer->get_health(), 150);
 }
 
 TEST(HunterTest, changeHealthWithZeroArg) {
     Adventurer* adventurer = new Hunter();
     adventurer->change_health(0);
-    EXPECT_EQ(adventurer->getHealth(), 200);
+    EXPECT_EQ(adventurer->get_health(), 200);
 }
 
 TEST(HunterTest, changeHealthToZeroHP) {
     Adventurer* adventurer = new Hunter();
     adventurer->change_health(-200);
-    EXPECT_EQ(adventurer->getHealth(), 0);
+    EXPECT_EQ(adventurer->get_health(), 0);
 }
 
 TEST(HunterTest, changeHealthToNegativeHP) {
     Adventurer* adventurer = new Hunter();
     adventurer->change_health(-250);
-    EXPECT_EQ(adventurer->getHealth(), 0);
+    EXPECT_EQ(adventurer->get_health(), 0);
 }
 
 TEST(HunterTest, getHealthFullHP){
     Adventurer* adventurer = new Hunter();
-    EXPECT_EQ(adventurer->getHealth(), 200);
+    EXPECT_EQ(adventurer->get_health(), 200);
 }
 
 TEST(HunterTest, getHealthZeroHP){
     Adventurer* adventurer = new Hunter();
     adventurer->change_health(-200);
-    EXPECT_EQ(adventurer->getHealth(), 0);
+    EXPECT_EQ(adventurer->get_health(), 0);
 }
 
 TEST(HunterTest, getHealthAtDeath) {
     Adventurer* adventurer = new Hunter();
     adventurer->change_health(-150);
     adventurer->change_health(-100);
-    EXPECT_EQ(adventurer->getHealth(), 0);
+    EXPECT_EQ(adventurer->get_health(), 0);
 }
 
 #endif
