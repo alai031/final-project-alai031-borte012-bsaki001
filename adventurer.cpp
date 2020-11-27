fd302dca140d258e6c860020d11b243866496bd0
briortega <briortega14@gmail.com>
2020-11-26 19:15:40 -0800

    Added adventurer files for testing

---
+++ adventurer.cpp
@@ -0,0 +1,25 @@
+#include "adventurer.hpp"
+
+Adventurer::Adventurer()
+{
+        this->health = health;
+        this->damage = damage;
+}
+
+void Adventurer::change_health(int healthChange)
+{
+        health -= healthChange;
+}
+
+void Adventurer::attackDamage(Monster* monster)
+{
+        monster->decrementHealth(damage);
+}
+
+int Adventurer::get_health()
+{
+        return health;
+}
+void Adventurer::attackMessage(){
+        std::cout << "Adventurer's default attack message " << std::endl;
+}
---
+++ adventurer.hpp
@@ -0,0 +1,20 @@
+#ifndef ADVENTURER_HPP
+#define ADVENTURER_HPP
+
+#include <iostream>
+#include "mock_monster.hpp"
+
+class Adventurer{
+protected:
+        int health = 0;
+        int damage = 0;
+public:
+        Adventurer();
+        virtual void attackMessage();
+        void change_health(int healthChange);
+        void attackDamage(Monster* monster);
+        int get_health();
+
+};
+
+#endif
