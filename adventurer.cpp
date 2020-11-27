321d5c15892691cbf6f114c83767de3badec5f5b
briortega <briortega14@gmail.com>
2020-11-26 18:15:10 -0800

    Added if statment to set helth to 0 if negative

--- adventurer.cpp
+++ adventurer.cpp
@@ -9,6 +9,9 @@ Adventurer::Adventurer()
 void Adventurer::change_health(int healthChange)
 {
 	health -= healthChange;
+	if (health < 0){
+		health = 0;
+	}
 }
 
 void Adventurer::attackDamage(Monster* monster)
