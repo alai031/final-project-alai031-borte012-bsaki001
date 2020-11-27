e4b1f26d8ffdfc05f112643e986f4066b9d3ee4e
briortega <briortega14@gmail.com>
2020-11-26 22:48:27 -0800

    Modify adventurer class and mock monster class

--- mock_monster.hpp
+++ mock_monster.hpp
@@ -1,11 +1,18 @@
 #ifndef MONSTER_HPP
 #define MONSTER_HPP
 
+#include "adventurer.hpp"
+
+class Adventurer;
+
 class Monster {
 private:
 	int health;
+	int damage;
 public:
-	Monster(int health);
+	Monster(int health, int damage);
 	void decrementHealth(int damage);
+	int get_health();
+	void attack(Adventurer* adventurer);
 };
 #endif
