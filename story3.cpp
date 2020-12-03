c33860aa016c9d479ad34f42b011e6ea2707d808
alai031 <alai031@ucr.edu>
2020-12-02 19:20:29 -0800

    Added changes to story3.cpp and changed story3 title in main.cpp

--- story3.cpp
+++ story3.cpp
@@ -7,14 +7,20 @@
 using namespace std;
 
 void Story3::story(){
-	cout << "The adventurer has been assigned by the King to travel to a place called Concord and deliver an important message on his behalf." << endl;
-	cout << "The adventurer has been told that there is little to worry about on your journey, but you clearly see in front of you traps and monsters." << endl;
-	cout << "If the adventurer survives, the King promises a promotion of being his right-hand man. If the adventurer dies, the King says 'nothing will be lost'." << endl;
-	cout << "Choose the adventurer's path and ensure his survival!" << endl;	 
+	cout << "The adventurer has been ordered by the King to travel to Concord and deliver an important message on his behalf." << endl;
+	cout << "The adventurer has been told that there is little to worry about on the journey, but he clearly sees in front of him numerous traps and monsters." << endl;
+	cout << "If the adventurer survives, the King promises a promotion of being his right-hand man. If the adventurer dies, the King says that 'nothing will be lost'." << endl;
+	cout << "Choose the adventurer's paths and ensure his survival!" << endl;	 
 	while (counter != 0){
-		int userInput = 0;
-		cout << "Choose a number 0 - 1 which decides whether the adventurer goes on Path 0 or Path 1." << endl;
-		cin >> userInput;
+		int userInput = -1;
+		while (userInput != 0 && userInput != 1){
+			cout << "Choose a number 0 - 1 which decides whether the adventurer goes on Path 0 or Path 1." << endl;
+	                cin >> userInput;
+			if (userInput != 0 && userInput != 1){
+				cout << "Invalid input. Please try again." << endl;
+			}
+		}
+		
 		int randNum = rand() % 2;
 
 		if (userInput == randNum){
@@ -22,7 +28,9 @@ void Story3::story(){
 			while (adventurer->get_health() != 0 && newMonster->getHealth() > 0){
 				adventurer->attackMessage();
 				adventurer->attackDamage(newMonster);
-				newMonster->damage();
+				if (newMonster->getHealth() > 0){
+					newMonster->damage();
+				}
 			}
 		}
 
@@ -34,13 +42,13 @@ void Story3::story(){
 		--counter;
 
 		if (adventurer->get_health() == 0){
-			cout << "The adventurer gasps his last breath of air and dies. You lose." << endl;
+				cout << "The adventurer takes his last breath of air and dies. You lose." << endl;
 			counter = 0;
 		}	
 	}
 
         if (adventurer->get_health() != 0) {
-                cout << "The adventurer lives to tell the tale and breathes a sigh of relief. The King is delighted by your heroism and strength." << endl;
+                cout << "The adventurer lives to tell the tale and breathes a sigh of relief. He successfully delivers the message and the King is delighted." << endl;
                 cout << "The adventurer is granted the promotion that the King promised. You win!" << endl;
         }
 
